5ba738ccaad32438c8164b2f56f40346f95d2acb
DrZuipperpips <arturokfya@gmail.com>
2022-01-28 16:54:03 -0300

    somethingsomethinglblaasfld

---
+++ experimentos/nodejs/logger.js
@@ -0,0 +1,6 @@
+//(function (exports, require, module, __filename, __dirname) {
+module.exports.log = (message) => {
+  console.log(message);
+};
+module.exports.endpoint = "http://mylogger.io/log"
+//})
