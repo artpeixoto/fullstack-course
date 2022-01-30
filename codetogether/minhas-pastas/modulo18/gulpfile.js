11c267823cabbba21f9a04c1b64b8dde64f41390
DrZuipperpips <arturokfya@gmail.com>
2022-01-30 15:40:10 -0300

    aprendi um pouco de gulp, acho.  No ramo modulo18  Mudanças a serem submetidas: 	modified:   .gitignore 	new file:   gulpfile.js 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json
     Changes not staged for commit:
    	modified:   ../modulo15/projeto/index.html
    	modified:   ../../repo-professor (conteúdo modificado)
    
     Arquivos não monitorados:
    	../modulo15/node_modules/
    	../modulo15/package-lock.json
    	../modulo15/package.json
    	../modulo15/projeto/gulpfile.js

---
+++ codetogether/minhas-pastas/modulo18/gulpfile.js
@@ -0,0 +1,25 @@
+const gulp =        require('gulp');
+const gulpCli =     require('gulp-cli');
+const gulpRename =  require('gulp-rename');
+const gulpCssMin =  require('gulp-cssmin');
+const gulpCat  =    require('gulp-concat');
+const gulpUglify =  require('gulp-uglify')
+const importSource = './node_modules'
+function sendCssIGuess(cb){
+  return (
+    gulp.src(   importSource + '/**/*.css' )
+        .pipe(  gulpCat('libs.css') )
+        .pipe(  gulpCssMin()   )
+        .pipe(  gulpRename( {suffix: '.min'}  ) )
+        .pipe(  gulp.dest(  './dist/css'  ) )
+    );
+}
+
+function tarefasJs(cb) {
+  return (gulp 
+              .src(importSource + '/**/*.js' )
+              .pipe(gulpCat('libs.js'))
+              .pipe(gulp.dest('./dist/js/')))
+}
+exports.scripts = tarefasJs;
+exports.styles  = sendCssIGuess;
