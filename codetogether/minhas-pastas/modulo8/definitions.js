2860fc6e004d4376baec10ce82d2eb539023c50c
DrZuipperpips <arturokfya@gmail.com>
2022-02-04 12:07:13 -0300

    removed useless folder, added support folder
    fold "old" has been relocated outside here. The project is valid, but
    here is not its place.
    
    Also, added the classes support folder. Im gonna use it anyway

--- codetogether/minhas-pastas/modulo8/definitions.js
+++
@@ -1,5 +0,0 @@
-os =    require('os');
-fs =    require('fs');
-path =  require('path')
-
-rootHtmlPages = /.src\/\.*.html/
--- codetogether/minhas-pastas/modulo8/gulpfile.js
+++ codetogether/minhas-pastas/modulo8/gulpfile.js
@@ -1,99 +1,61 @@
-const gulp =        require('gulp');
-const browserSync = require('browser-sync');
-const sass =        require('gulp-sass')(require('sass'));
-const os =          require('os');
-const path =        require('path')
-const htmlImport =  require('gulp-html-import');
-const httpServer =  require('http-server');
-const Json =        JSON;
+const {src, dest,  parallel , series, task, pipe, watch} =  require("gulp");
+const path =                                                require('path')
+const htmlImport =                                          require('gulp-html-import')
+const sass =                                                require('gulp-sass')(require('sass'))
+const browserSync = require('browser-sync').create();
 
-const p = {
-  //sources are the things i manually code that end up being in the final result
-  sources: {
-      _path: "./src"
-      , htmlRootPages: {
-        _path: "/**/*.html" 
-      }
-      , styles: {
-        _path: "style/**/*.scss"
-        }
-    }
-  , build: {
-//    procedure: (source, buildProcesses[source]) => {(buildProcesses(source))}
-     _path: "./build"
-  }
-  , use: {
-    procedure: (pages, browser) => {browser(pages)} 
-    
-  }  
+anyScss = "*.scss"
+anywhere = "**/"
+anyHtml = "*.html"
+root = "./"
+_src = root + "src/"
+src_style = _src + "style/"
+src_contents = _src + "contents/"
+build = root + "build/"
+build_style = build+"style/"
+
+buildSassProc = (srcFile, destDir) =>
+{
+   console.log(`Building style file:\n\tSource: ${srcFile}\n\tDestination: ${destDir}`);
+   src(srcFile)
+   .pipe(sass().on("error", sass.logError))
+   .pipe(dest(destDir.slice(0,-1)));
+}
+buildHtmlProc = (srcFile, importsDir, destDir) => {
+   console.log(`Building content (HTML) file:\n\tsource: ${srcFile}\n\timports directory: ${importsDir}\n\tDestination: ${destDir}\n`);
+   src(srcFile).
+   pipe(htmlImport(importsDir)).
+   pipe(dest(destDir.slice(0, -1)));
 }
 
-const sourceDir = './src'
-const buildDir  = './build'
-const stylesSourceDir = path.join(sourceDir,      'style');
-const styleBuildDir  = path.join(buildDir,   "style");
-const htmlSourceDir  =  sourceDir;
-const htmlSourceComponentsDir = path.join(htmlSourceDir, "html/")
-const htmlSourceCompPatt = htmlSourceComponentsDir + "*.html"
+getWatcher = (watching, buildFunc) =>{
+   let contentsWatcher = watch(watching)
+   console.log(`watching: ${watching}`)
+   contentsWatcher.on("change", buildFunc)
+   return contentsWatcher
+} 
 
-
-
-patterns = {
-  style: path.join(stylesSourceDir, "**.scss")
-  , html: path.join(htmlSourceDir, "*.html")
-  };
-
-
-function buildHtml(cb){
-  srcPattern = patterns.html;
-  destDir    = "./build/"
-  
-  console.log(`Building contents:\t${srcPattern} ->\t${destDir}\n...`)
-  
-  gulp.src(srcPattern)
-      .pipe(htmlImport(htmlSourceComponentsDir))
-      .pipe(gulp.dest(destDir))
-  }
-
-
-function buildStyles(cb) {
-  srcPattern = patterns.style;
-  destDir    = "./build/style/"
-  
-  console.log(`Building styles:\t${srcPattern} ->\t${destDir}`)
-  
-  gulp.src( patterns.style ) 
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest("./build/style/"));
-  }
-
-
-function watchSource() {
-  watched = {
-    style: [patterns.style]
-    , html: [patterns.html, htmlSourceCompPatt]
-  } 
-  console.log(`Watching:\n\t${Json.stringify(watched)}`);
-  watchers = 
-    { style: gulp.watch(watched.style)
-    , html:  gulp.watch(watched.html) }
-
-  watchers.style.on("change", (path, stats) => {
-    console.log(`detected the changes in the file: ${path} ${Json.stringify(stats)}`)
-    buildStyles();
-  });
-
-  watchers.html.on("change", (path, stats) => {
-    console.log(`detected the changes in the file: ${path} ${Json.stringify(stats)}`)
-    buildHtml();
-  });
+getContentsWatcher = () => {
+   let srcPat = _src + anyHtml;
+   let imports = src_contents;
+   let _dest = build;
+   watching = [srcPat, imports + anyHtml]
+   console.log(`\nInstantiating contents watcher. Arguments:\n\tSource pattern: ${srcPat}\n\tImports dir: ${imports}\n\tDestination: ${_dest}\n`);
+   return  getWatcher(watching, () => buildHtmlProc(srcPat, imports, _dest)) 
+}
+getStylesWatcher = () => {
+   let srcPat = src_style + anyScss;
+   let _dest = build_style;
+   let watching = [srcPat]
+   console.log(`Instantiating styles watcher. Arguments:\n\tSource pattern: ${srcPat}\n\tDestination: ${_dest}\n`);
+   return  getWatcher(watching, () => buildSassProc(srcPat,_dest )) 
 }
 
-function startDebugServer(cb){
-  console.log('starting debug Server')
-  gulp.pipe(httpServer());
-}
-module.exports.watch =        watchSource;
-module.exports.buildStyles =  buildStyles
-module.exports.buildHtml =    buildHtml
-module.exports.build =        gulp.series( buildHtml, buildStyles);
+module.exports.startServer =   parallel(() => {browserSync.init({server:{baseDir:"./build"}})}, () => {watch([build + "**"]).on("change", browserSync.reload)})
+module.exports.watchContents = getContentsWatcher;
+module.exports.watchContents.displayName = 'watch:contents'
+module.exports.watchContents.description = "will instantiate a watcher and builder for the contents(html) files"
+module.exports.watchStyles =   getStylesWatcher;
+module.exports.watchStyles.displayName = 'watch:styles'
+module.exports.watchStyles.description = "will instantiate a watcher and builder for the styles files"
+module.exports.watch = parallel(module.exports.watchStyles, module.exports.watchContents) 
